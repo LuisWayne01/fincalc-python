a9731751976a2c7885f9973e609803120a69a77a
LuisWayne01 <luisdeadpool39@gmail.com>
2026-09-01 16:56:41 +0000

    fix: correção de sintaxe

--- fincalc.py
+++ fincalc.py
@@ -16,7 +16,7 @@ def calcular_aposentadoria(patrimonio_atual: float, aporte_mensal: float, anos: 
 
 
 
-def calcular_juros_compostos(capital: float, taxa_anual: float, anos: int) -> float 
+def calcular_juros_compostos(capital: float, taxa_anual: float, anos: int) -> float:
     """Calcular o montante final obtido por juros compostos"""
     montante = capital * ((1 + (taxa_anual / 100) ** anos))
     return montante
@@ -24,7 +24,7 @@ def calcular_juros_compostos(capital: float, taxa_anual: float, anos: int) -> fl
     if __name__ == "__main__":
         print("Iniciando o sistema FinCalc...")
         parimmonio = calcular_aposentadoria(10000.0, 500.0, 20, 6.0)
-        print(f"Patrimônio Estimado para Aposentadoria: R$ {parimmonio:.2f}")S
+        print(f"Patrimônio Estimado para Aposentadoria: R$ {parimmonio:.2f}")
         montante = calcular_juros_simples(1000.0, 5.0, 2)
         print (f"Juros Simples: R$ {montante:.2f}")
         montante_comp = calcular_juros_compostos(1000.0, 5.0, 2)
