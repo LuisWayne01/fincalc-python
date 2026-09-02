605f9f69e721de2b423088c161eeb96cd70ea0ff
Júlio Henrique Amorim <164095984+juli0amorim@users.noreply.github.com>
2026-09-02 12:07:31 +0000

    fix: corrige erros de formatação do flake8

--- fincalc.py
+++ fincalc.py
@@ -24,17 +24,16 @@ def calcular_juros_compostos(capital: float, taxa_anual: float, anos: int) -> fl
 
 
 def calcular_irrf(salario_bruto: float) -> float:
-  
-  if salario_bruto <= 2259.20:
-    return  0.0
-  elif salario_bruto <= 2826.65:
-    return (salario_bruto * 0.075) - 169.44
-  elif salario_bruto <= 3751.05:
-    return (salario_bruto * 0.15) - 381.44
-  else: 
-    return (salario_bruto * 0.225) - 662.77
+    """Calcular o imposto de renda retido na fonte (IRRF)."""
+    if salario_bruto <= 2259.20:
+        return 0.0
+    elif salario_bruto <= 2826.65:
+        return (salario_bruto * 0.075) - 169.44
+    elif salario_bruto <= 3751.05:
+        return (salario_bruto * 0.15) - 381.44
+    else:
+        return (salario_bruto * 0.225) - 662.77
 
-  
     if __name__ == "__main__":
         print("Iniciando o sistema FinCalc...")
         parimmonio = calcular_aposentadoria(10000.0, 500.0, 20, 6.0)
