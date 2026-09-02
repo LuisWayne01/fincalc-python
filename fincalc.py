01da077883b9c5214894499e17bf0be859d5dad9
Júlio Henrique Amorim <164095984+juli0amorim@users.noreply.github.com>
2026-09-02 11:35:57 +0000

    feat: adiciona função de cálculo de IRRF

--- fincalc.py
+++ fincalc.py
@@ -30,3 +30,14 @@ def calcular_juros_compostos(capital: float, taxa_anual: float, anos: int) -> fl
         print(f"Juros Simples: R$ {montante:.2f}")
         montante_comp = calcular_juros_compostos(1000.0, 5.0, 2)
         print(f"Juros Compostos: R${montante_comp:.2f}")
+
+def calcular_irrf(salario_bruto: float) -> float:
+  
+  if salario_bruto <= 2259.20:
+    return  0.0
+  elif salario_bruto <= 2826.65:
+    return (salario_bruto * 0.075) - 169.44
+  elif salario_bruto <= 3751.05:
+    return (salario_bruto * 0.15) - 381.44
+  else: 
+    return (salario_bruto * 0.225) - 662.77
